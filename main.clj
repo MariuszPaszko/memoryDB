9573269a7800f9527c85c79394f683ab30d61079
pashunio <mariusz.paszko.2010@gmail.com>
2020-11-09 00:08:08 +0000

    Initial commit

---
+++ main.clj
@@ -0,0 +1,10 @@
+(def memory-db (atom {}))
+(defn read-db [] @memory-db)
+(defn write-db [new-db] (reset! memory-db new-db))
+(defn add-table [table-name]
+  (let [db (read-db)]
+    (write-db 
+      (assoc db table-name {:name [] :indexes {}}))))
+ (defn drop-table [table-name]
+  (let [db (read-db)]
+    (write-db (dissoc db table-name))))
